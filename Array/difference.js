9fb5cd8ba9c05fd1f4fe36566d253c93bbfac0d3
DionysusBenstein <dionysusbenstein@gmail.com>
2021-10-07 14:02:21 +0300

    Change file structure

--- Array/difference.js
+++ Array/difference.js
@@ -4,7 +4,7 @@
 function difference(arr1, arr2) {
     const result = [];
 
-    for (const item of arr1) {        
+    for (const item of arr1) {       
         if (!arr2.includes(item)) {
             result.push(item);
         }
@@ -12,5 +12,3 @@ function difference(arr1, arr2) {
 
     return result;
 }
-
-console.log(difference([2, 1], [2, 3]));
---
+++ Array/differenceBy.js
@@ -0,0 +1,12 @@
+// differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor);
+// => [1.2]
+ 
+// differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x');
+// => [{ 'x': 2 }]
+
+function differenceBy(arr, iter) {
+    
+}
+
+console.log(differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor));
+console.log(differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'));

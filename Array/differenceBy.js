dda00cc86d8cb9436d466629a6373145b6d88692
DionysusBenstein <dionysusbenstein@gmail.com>
2021-11-03 14:19:14 +0300

    Add drop function

--- Array/differenceBy.js
+++ Array/differenceBy.js
@@ -4,9 +4,20 @@
 // differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x');
 // => [{ 'x': 2 }]
 
-function differenceBy(arr, iter) {
-    
+function differenceBy(arr, iter, criterion) {
+    let result = [];
+
+    if (typeof criterion === 'function') {
+        for (const item of arr) {
+            if (!iter.includes(criterion(item))) {
+                result.push(item);
+            }
+        }
+    }
+
+    return result;
 }
 
+console.log(typeof differenceBy);
 console.log(differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor));
 console.log(differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'));
